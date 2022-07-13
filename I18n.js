958f65de64324115d447c7b388013e2694710a54
Resul0316 <93797905+Resul0316@users.noreply.github.com>
2022-07-13 17:21:21 +0200

    Create I18n.js

---
+++ I18n.js
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./en.json";
+import it from "./it.json";
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  it: {
+    translation: it,
+  },
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en",
+    fallbackLng: "en",
+    debug: false,
+    interpolaration: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
